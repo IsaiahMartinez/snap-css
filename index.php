313a76a4b8a0cf0d38c794784ee3458957702fd1
Isaiah Martinez <imartinez32@cnm.edu>
2018-01-16 08:21:00 -0700

    added font styling

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<title>Snap-CSS</title>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	<body>
+		<p class="bold">Today is the beginning of a new school week.</p>
+		<p class="bold">We are learning CSS styling.</p>
+		<p id="highlight">The end.</p>
+	</body>
+</html>
